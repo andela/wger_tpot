661a5986eb57e54caae1ebfd9af2d02a876d8c1e
pythonGeek <stephen.kanyi@andela.com>
2017-02-07 16:06:20 +0300

    Removes the white space

--- wger/exercises/api/serializers.py
+++ wger/exercises/api/serializers.py
@@ -61,8 +61,6 @@ class ExerciseInfoSerializer(serializers.ModelSerializer):
     '''
     Exercise serializer
     '''
-    image = serializers.ImageField(source='main_image')
-    category = serializers.CharField(source='name', read_only=True)
     class Meta:
         model = Exercise
         depth = 1
